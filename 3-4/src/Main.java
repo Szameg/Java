8ce25afa3e49b8fd0114bd4f41dc0595d81c196b
Kamilla <kovkamac@gmail.com>
2021-06-09 00:34:34 +0200

    Matrix was added

--- 3-4/src/Main.java
+++ 3-4/src/Main.java
@@ -1,18 +1,45 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void whichIsLonger(int[] numbersOne, int[] numbersTwo) {
         if (numbersOne.length > numbersTwo.length) {
-            System.out.println("The first array is longer which has " +numbersOne.length+ " elements.");
+            System.out.println("The first array is longer which has " + numbersOne.length + " elements.");
         } else if (numbersTwo.length > numbersOne.length) {
-            System.out.println("The second array is longer which has " +numbersTwo.length+ " elements.");
+            System.out.println("The second array is longer which has " + numbersTwo.length + " elements.");
         } else {
-            System.out.println("The two arrays' length equal. They have " +numbersOne.length+ " elements.");
+            System.out.println("The two arrays' length equal. They have " + numbersOne.length + " elements.");
+        }
+    }
+
+    public static void matrixMaker(int number) {
+        try {
+            if (number == 0) {
+                throw new Exception();
+            }
+            int[][] matrix = new int[number][number];
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    if (i == j) {
+                        matrix[i][j] = number;
+                    } else {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+            for (int i = 0; i < matrix.length; i++) {
+                System.out.println(Arrays.toString(matrix[i]));
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        int[] numOne = new int[] {1,2};
-        int[] numTwo = new int[] {4,5,6};
-        whichIsLonger(numOne,numTwo);
+        int[] numOne = new int[]{1, 2};
+        int[] numTwo = new int[]{4, 5, 6};
+        whichIsLonger(numOne, numTwo);
+
+        matrixMaker(5);
     }
 }
